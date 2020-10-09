9c3ac6a3e77d6979d5df643cad81116ce594c7b9
unknown <sudupelaura@gmail.com>
2020-10-09 11:45:21 +0200

    exercise 3 finished

--- LAB2/LAB2_3ejercicio.R
+++ LAB2/LAB2_3ejercicio.R
@@ -8,21 +8,31 @@ library(MASS) #cargamos los paquetes
 library(datasets)
 data(package='MASS')
 data(package='datasets') #ambos dataframes estan en esta libreria, ORANGE e IRIS
-orange <- data('Orange') #vemos la data
-orange
-iris <- data('iris')
-iris
-
-head(orange)
+data('Orange') #vemos la data
+head(Orange)
+data('iris')
 head(iris)
 
 #. (a) Buscar un resumen de las variables de cada dataset.
-summary_orange <- summary(orange)
-summary_orange
+summary(Orange)
+summary(iris)
+
 summary_iris <- summary(iris)
 summary_iris
+
 #. (b) Generar una tabla de frecuencias absolutas y una de relativas con
 #cualquier variable del dataset.
+#frecuencia absoluta
+table(Orange$age) 
+table(iris$Sepal.Length)
+#frecuencia relativa
+prop.table(Orange$age)
+prop.table(iris$Sepal.Length)
+
 #. (c) Generar una tabla de frecuencias absolutas con cada una de las variables
 #del conjunto de datos Orange. ¿Todas las tablas generadas tienen sentido
 #para vosotros?
+table(Orange$Tree)
+table(Orange$age)
+table(Orange$circumference)
+#Con la frecuencia absoluta de three no obtenemos ningun dato concluyente
