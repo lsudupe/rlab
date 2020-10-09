04884a5156fd5b6f2e6524bc2309e131e633de67
unknown <sudupelaura@gmail.com>
2020-10-09 18:36:31 +0200

    exercise 6 finished, 7 started

---
+++ LAB2/LAB2_7ejercicio.R
@@ -0,0 +1,7 @@
+#Ejercicio 7.Representad gráficamente los datos contenidos en los
+#conjuntos de datos (incorporados en UsingR) bumpers, firstchi, math 
+#con un histograma y/o un boxplot usando ggplot2.
+library(UsingR)
+a <- data.frame(bumpers)
+b <- data.frame(firstchi)
+c <- data.frame(match)
