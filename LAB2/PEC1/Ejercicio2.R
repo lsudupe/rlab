7c120cfbc8b8a565aa4b0a80062c382944a0fd40
unknown <sudupelaura@gmail.com>
2020-10-12 12:34:35 +0200

    just started PEC1 exercise 2

---
+++ LAB2/PEC1/Ejercicio2.R
@@ -0,0 +1,4 @@
+#Ejercicio 2.Definid un data frame, dataHealthHeart_w, y otro data frame, 
+#dataHealthHeart_m, que corresponderán a los conjuntos de datos de las mujeres y
+#de los hombres, respectivamente. Posteriormente, exportad ambos data frame a 
+#dos ficheros (HealthHeart_w,HealthHeart_m) de tipo csv.
