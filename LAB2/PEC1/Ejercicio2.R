d181fd8a4d55bf754762d7642f9fa9eae5d76754
unknown <sudupelaura@gmail.com>
2020-10-13 11:26:23 +0200

    PEC1 exercise 2 done

--- LAB2/PEC1/Ejercicio2.R
+++ LAB2/PEC1/Ejercicio2.R
@@ -2,3 +2,19 @@
 #dataHealthHeart_m, que corresponderán a los conjuntos de datos de las mujeres y
 #de los hombres, respectivamente. Posteriormente, exportad ambos data frame a 
 #dos ficheros (HealthHeart_w,HealthHeart_m) de tipo csv.
+
+health_heart <- read.csv("/Users/lsudu/code/rlab/LAB2/PEC1/heart.csv")
+health_heart$sex <- factor(health_heart$sex, levels=c("0","1"),
+                       labels=c("male","female"))
+
+dataHealthHeart_w <- data.frame(subset(health_heart, 
+                                       health_heart$sex == "female"))
+dataHealthHeart_m <- data.frame(subset(health_heart, 
+                                health_heart$sex == "male"))
+
+write.csv(dataHealthHeart_w, file=
+            "/Users/lsudu/code/rlab/LAB2/PEC1/HealthHeart_w.csv")
+        
+write.csv(dataHealthHeart_m, file =
+            "/Users/lsudu/code/rlab/LAB2/PEC1/HealthHeart_m.csv")
+
