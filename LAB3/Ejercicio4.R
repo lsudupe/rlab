533762668f22501eefdd13c7bc64189035b5e2bd
unknown <sudupelaura@gmail.com>
2020-10-26 10:36:07 +0100

    exercidese 5 done, issue with 'j' variable

--- LAB3/Ejercicio4.R
+++ LAB3/Ejercicio4.R
@@ -12,3 +12,28 @@ for(i in 1:length(bwt))
     count <- count + 1
   }
 count
+
+
+#Ejercicio 5
+
+#definimos el vector que guardara los pesos
+bwt_c <- c()
+j <- 1
+
+#deginimos la función
+F_bwt <- function(){
+  for (i in 1:length(birthwt$bwt))
+    if (birthwt$age[i] < 20) {
+      bwt_c[j] <- birthwt$bwt[i]
+      j <- j + 1
+    }
+  return (bwt_c)
+  }
+
+bwt_c_edad <- F_bwt()
+bwt_c_edad
+
+
+#calculamos la media y el grafico
+mean(bwt_c_edad)
+plot(bwt_c_edad)
